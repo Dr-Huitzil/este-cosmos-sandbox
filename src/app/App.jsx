9eef3b74fda90cf121e02afe0a4f4b93da519f66
Dr_Huitzil <gotenkscoolness300@gmail.com>
2026-04-07 21:08:47 -0700

    first commit

---
+++ src/app/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// We will import your main window here soon!
+import './index.css';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', minHeight: '100vh', backgroundColor: '#333' }}>
+       {/* <FuelTrackerWindow /> will go here */}
+    </div>
+  )
+}
+
+export default App;
