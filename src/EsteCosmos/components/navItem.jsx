91dcecaceea2bfebf9ee573feb0a8797474c78bc
Dr_Huitzil <gotenkscoolness300@gmail.com>
2026-04-10 23:59:21 -0700

    navItem was added. it controls the way navigation looks on fullscreen and mobile

---
+++ src/EsteCosmos/components/navItem.jsx
@@ -0,0 +1,39 @@
+import { memo } from "react";
+import styles from "./navItem.module.css";
+
+/**
+ * Sidebar desktop navigation item
+ * @param {{ icon: React.ReactNode, label: string, active: boolean, onClick: function}} props
+ */
+export const NavItem = memo(function NavItem({ icon, label, active, oClick }) {
+  return (
+    <button
+      onClick={onClick}
+      className={`${styles.navItem} ${active ? styles.navItemActive : ""}`}
+    >
+      <span className={styles.navIcon}>{icon}</span>
+      <span className={styles.navLabel}>{label}</span>
+    </button>
+  );
+});
+
+/**
+ * Bottom mobile navigation item
+ * @param {{icon: React.ReactNode, label: string, active: boolean, onClick: function}} props
+ */
+export const MobileNavItem = memo(function MobileNavItem({
+  icon,
+  label,
+  active,
+  onClick,
+}) {
+  return (
+    <button
+      onClick={onClick}
+      className={`${styles.mobileNavItem} ${active ? styles.mobileNavItemActive : ""}`}
+    >
+      {icon}
+      <span className={styles.mobileNavLabel}>{label}</span>
+    </button>
+  );
+});
